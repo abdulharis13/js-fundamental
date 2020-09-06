1a827eb1f9e034ca94e05e80412c6edd3a8b78b3
abdulharis13 <aharis1397@gmail.com>
2020-09-06 16:22:40 +0700

    first commit

---
+++ pr2.js
@@ -0,0 +1,8 @@
+let nama = "Pemrograman";
+let tampung='';
+// membuat segitiga siku-siku
+    for (let k = 0; k < nama.length; k++) {
+        tampung += nama[k];
+                console.log(tampung);
+            }
+    
