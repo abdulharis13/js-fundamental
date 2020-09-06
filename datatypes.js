4b845939428888103eeac8d32f9cc3384d35c98d
abdulharis13 <aharis1397@gmail.com>
2020-09-06 16:03:55 +0700

    first commit

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+const nama = 'M abdul haris';//string
+let umur = 18;//integer
+var alamat ='pondok kopi';//string
+let lulus = true; //boolean  
+    
+    console.log(`saya ${nama} umur ${umur} alamat ${alamat}`);
