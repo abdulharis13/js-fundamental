a9b86db99f7c2bb3cab2f8e13cca48f2c7fda76d
abdulharis13 <aharis1397@gmail.com>
2020-09-07 09:27:52 +0700

    first commit

---
+++ pr.js
@@ -0,0 +1,16 @@
+// TUGAS 
+let nama = "Pemrograman";
+let tampung='';
+// membuat segitiga siku-siku
+    for (let k = 0; k < nama.length; k++) {
+        tampung += nama[k];
+                console.log(tampung);
+            }
+            console.log('/n');
+// TUGAS2 6 sept 2020
+let char = 'Pemrograman';
+let tampung2 = '';
+    for (let i = char.length-1; i >= 0; i--) {
+        tampung2 += char[i];
+        console.log(tampung2);
+    }
